cad2296a60a9b18b933ae7474297d3b86f1867d9
tmadhuker <tmadhuker@gmail.com>
2020-08-27 13:11:16 +0530

    hey

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
 
 app.get("/", (req, res) => {
-  res.send("welcome to home ");
+  res.send("Updated");
 });
+
+const port = process.env.port || 3000;
 app.listen(port, () => {
-  console.log("wassup");
+  console.log("Wazzappppp");
 });
